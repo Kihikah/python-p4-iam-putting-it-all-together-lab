7e220567db4a78bf9c697065afdfc0f40c336955
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-06-19 20:55:08 +0300

    Putting it All Together: IAM Lab

--- server/testing/models_testing/recipe_test.py
+++ server/testing/models_testing/recipe_test.py
@@ -3,46 +3,37 @@ from sqlalchemy.exc import IntegrityError
 
 from app import app
 from models import db, Recipe
+from models import db, Recipe, User
 
 class TestRecipe:
     '''User in models.py'''
 
     def test_has_attributes(self):
-        '''has attributes title, instructions, and minutes_to_complete.'''
-        
         with app.app_context():
-
             Recipe.query.delete()
+            User.query.delete()
+            db.session.commit()
+
+            user = User(username="chef123")
+            user.password_hash = "secret123"
+            db.session.add(user)
             db.session.commit()
 
             recipe = Recipe(
-                    title="Delicious Shed Ham",
-                    instructions="""Or kind rest bred with am shed then. In""" + \
-                        """ raptures building an bringing be. Elderly is detract""" + \
-                        """ tedious assured private so to visited. Do travelling""" + \
-                        """ companions contrasted it. Mistress strongly remember""" + \
-                        """ up to. Ham him compass you proceed calling detract.""" + \
-                        """ Better of always missed we person mr. September""" + \
-                        """ smallness northward situation few her certainty""" + \
-                        """ something.""",
-                    minutes_to_complete=60,
-                    )
+            title="Delicious Shed Ham",
+            instructions="This is a test instruction with more than fifty characters.",
+            minutes_to_complete=60,
+            user_id=user.id
+            )
 
+            
             db.session.add(recipe)
             db.session.commit()
 
-            new_recipe = Recipe.query.filter(Recipe.title == "Delicious Shed Ham").first()
-
+            new_recipe = Recipe.query.filter_by(title="Delicious Shed Ham").first()
             assert new_recipe.title == "Delicious Shed Ham"
-            assert new_recipe.instructions == """Or kind rest bred with am shed then. In""" + \
-                    """ raptures building an bringing be. Elderly is detract""" + \
-                    """ tedious assured private so to visited. Do travelling""" + \
-                    """ companions contrasted it. Mistress strongly remember""" + \
-                    """ up to. Ham him compass you proceed calling detract.""" + \
-                    """ Better of always missed we person mr. September""" + \
-                    """ smallness northward situation few her certainty""" + \
-                    """ something."""
             assert new_recipe.minutes_to_complete == 60
+
 
     def test_requires_title(self):
         '''requires each record to have a title.'''
--- server/testing/models_testing/user_test.py
+++ server/testing/models_testing/user_test.py
@@ -8,51 +8,26 @@ class TestUser:
     '''User in models.py'''
 
     def test_has_attributes(self):
-        '''has attributes username, _password_hash, image_url, and bio.'''
-        
         with app.app_context():
-
             User.query.delete()
             db.session.commit()
 
             user = User(
                 username="Liz",
-                image_url="https://prod-images.tcm.com/Master-Profile-Images/ElizabethTaylor.jpg",
-                bio="""Dame Elizabeth Rosemond Taylor DBE (February 27, 1932""" + \
-                    """ - March 23, 2011) was a British-American actress. """ + \
-                    """She began her career as a child actress in the early""" + \
-                    """ 1940s and was one of the most popular stars of """ + \
-                    """classical Hollywood cinema in the 1950s. She then""" + \
-                    """ became the world's highest paid movie star in the """ + \
-                    """1960s, remaining a well-known public figure for the """ + \
-                    """rest of her life. In 1999, the American Film Institute""" + \
-                    """ named her the seventh-greatest female screen legend """ + \
-                    """of Classic Hollywood cinema."""
+                image_url="https://img.url/elizabeth.jpg",
+                bio="Famous actress..."
             )
+            user.password_hash = "secret"
 
-            user.password_hash = "whosafraidofvirginiawoolf"
-            
             db.session.add(user)
             db.session.commit()
 
-            created_user = User.query.filter(User.username == "Liz").first()
+            created_user = User.query.filter_by(username="Liz").first()
+            assert created_user.username == "Liz"
 
-            assert(created_user.username == "Liz")
-            assert(created_user.image_url == "https://prod-images.tcm.com/Master-Profile-Images/ElizabethTaylor.jpg")
-            assert(created_user.bio == \
-                """Dame Elizabeth Rosemond Taylor DBE (February 27, 1932""" + \
-                """ - March 23, 2011) was a British-American actress. """ + \
-                """She began her career as a child actress in the early""" + \
-                """ 1940s and was one of the most popular stars of """ + \
-                """classical Hollywood cinema in the 1950s. She then""" + \
-                """ became the world's highest paid movie star in the """ + \
-                """1960s, remaining a well-known public figure for the """ + \
-                """rest of her life. In 1999, the American Film Institute""" + \
-                """ named her the seventh-greatest female screen legend """ + \
-                """of Classic Hollywood cinema.""")
-            
             with pytest.raises(AttributeError):
-                created_user.password_hash
+                created_user.password_hash  # can't access this
+
 
     def test_requires_username(self):
         '''requires each record to have a username.'''
@@ -91,6 +66,7 @@ class TestUser:
             db.session.commit()
 
             user = User(username="Prabhdip")
+            user.password_hash = "safepassword123"
 
             recipe_1 = Recipe(
                 title="Delicious Shed Ham",
